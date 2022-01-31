1649152331a89ead2299a711fcf58e649c46c6fb
Marlontino <perry.niko7@gmail.com>
2022-01-31 14:34:17 -0800

    added Initial hangman game

---
+++ hangman.py
@@ -0,0 +1,39 @@
+import random, string
+from xml.sax.handler import all_properties
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words) # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        print('You have used thes letters: ', ' '.join(used_letters))
+
+        # current word (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+    
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
+
+hangman()
